e5ce0c5de6190926eb690c23e9e11f18dd11905c
Amir Eslamzadeh <amir@eslamzadeh.com>
2026-09-08 08:36:40 +0330

    fix: resolve Kafka publishing test types

--- tests/TCJ.Messaging.Kafka.Tests/KafkaIntegrationTests.cs
+++ tests/TCJ.Messaging.Kafka.Tests/KafkaIntegrationTests.cs
@@ -6,6 +6,7 @@ using TCJ.Messaging.Envelopes;
 using TCJ.Messaging.Extensions;
 using TCJ.Messaging.Kafka.Configuration;
 using TCJ.Messaging.Kafka.Extensions;
+using TCJ.Messaging.Kafka.Publishing;
 using TCJ.Messaging.Kafka.Receiving;
 using TCJ.Messaging.Publishing;
 using TCJ.Messaging.Receiving;
