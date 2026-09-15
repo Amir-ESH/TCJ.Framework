824f553b7f7e77596112b7e0b9d845cab5ded54a
Amir Eslamzadeh <amir@eslamzadeh.com>
2026-09-15 08:19:05 +0330

    fix: allow saga SQL Server tests to inspect inbox state

--- src/TCJ.EntityFrameworkCore/Properties/AssemblyInfo.cs
+++ src/TCJ.EntityFrameworkCore/Properties/AssemblyInfo.cs
@@ -7,3 +7,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("TCJ.Inbox.Tests")]
 
 [assembly: InternalsVisibleTo("TCJ.Messaging.Sagas.EntityFrameworkCore")]
+
+[assembly: InternalsVisibleTo("TCJ.Messaging.Sagas.SqlServer.Tests")]
